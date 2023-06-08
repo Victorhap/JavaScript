a6c40dd6043721c786380e1ba97a68734e7dd9dd
Victor Hebert <Victorbq18@gmail.com>
2023-06-08 02:12:39 -0300

    update

--- To-do-list/assets/js/script.js
+++ To-do-list/assets/js/script.js
@@ -6,6 +6,8 @@ const editForm = document.querySelector('#edit-form')
 const editInput = document.querySelector('#edit-input')
 const cancelEditBtn = document.querySelector('#cancel-edit-btn')
 
+let oldInputValue;
+
 // Funções
 const saveTodo = (text) => {
 
@@ -37,6 +39,25 @@ const saveTodo = (text) => {
     todoInput.focus();
 }
 
+const toggleForms = () => {
+    editForm.classList.toggle('hide')
+    todoForm.classList.toggle('hide')
+    todoList.classList.toggle('hide')
+}
+
+const updateTodo = (text) => {
+
+    const todos = document.querySelectorAll('.to-do')
+
+    todos.forEach((todo) => {
+
+        let todoTitle = todo.querySelector('h3')
+
+        if(todoTitle.innerText === oldInputValue) {
+            todoTitle.innerText = text;
+        }
+    })
+}
 
 // Eventos
 todoForm.addEventListener('submit', (e) =>{
@@ -48,4 +69,51 @@ todoForm.addEventListener('submit', (e) =>{
     if (inputValue){
         saveTodo(inputValue);
     }
+})
+
+document.addEventListener('click', (e) =>{
+
+    const targetEl = e.target
+    const parentEl = targetEl.closest('div')
+    let todoTitle;
+
+
+
+    if(parentEl && parentEl.querySelector('h3')){
+        todoTitle = parentEl.querySelector('h3').innerText;
+    }
+
+    if(targetEl.classList.contains('finish-to-do')) {
+        parentEl.classList.toggle('done')
+    }
+
+    if(targetEl.classList.contains('remove-to-do')) {
+        parentEl.remove();
+    }
+
+    if(targetEl.classList.contains('edit-to-do')) {
+        toggleForms();
+
+        editInput.value = todoTitle
+        oldInputValue = todoTitle
+    }
+})
+
+cancelEditBtn.addEventListener('click', (e) => {
+    e.preventDefault()
+
+    toggleForms();
+})
+
+editForm.addEventListener('submit', (e) => {
+
+    e.preventDefault();
+
+    const editInputValue = editInput.value
+
+    if(editInputValue) {
+        updateTodo(editInputValue)
+    }
+
+    toggleForms()
 })
