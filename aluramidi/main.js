63c4c793486086d800a0f3d3563de852a034f3f4
Victor Hebert <Victorbq18@gmail.com>
2023-06-13 19:32:55 -0300

    aluramidi

---
+++ aluramidi/main.js
@@ -0,0 +1,26 @@
+function tocaSom (idElementoAudio) {
+    document.querySelector(idElementoAudio).play();
+}
+
+const listaDeTeclas = document.querySelectorAll('.tecla')
+
+
+
+
+for (let contador = 0; contador < listaDeTeclas.length; contador ++){
+
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+    
+    tecla.onclick = function () {
+        tocaSom(`#som_${instrumento}`);   
+    }
+
+    tecla.onkeydown = function () {
+        tecla.classList.add('ativa');
+    }
+
+    tecla.onkeyup = function () {
+        tecla.classList.remove('ativa');
+    }
+}
